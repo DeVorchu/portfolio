b8a6175f21fd5f02bd42781ddcdd781ecebf1622
Vorczu <vorczu31337@gmail.com>
2022-01-29 13:13:49 +0100

    BANNER UPDATE
    + Timer
    + InfoBox

---
+++ src/components/ContentHandler/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ContentWrapper, PageWrapper } from '../../styles/conponents/content'
+
+export default function index() {
+    return (
+        <ContentWrapper>
+            <PageWrapper>
+
+            </PageWrapper>
+        </ContentWrapper>
+    )
+}
--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { LayoutWrapper } from '../../styles/globals/layout.styled'
+import  {LayoutWrapper } from '../../styles/globals/layout.styled'
 import Header from '../../components/Header'
 import Layout from '../../components/Footer'
+import Content from '../../components/ContentHandler'
 
 export default function index() {
     return (
         <LayoutWrapper>
             <Header />
+            <Content/>
             <Layout />
         </LayoutWrapper>
     )
